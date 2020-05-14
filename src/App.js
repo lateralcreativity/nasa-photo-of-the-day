f4eb53551e0c98663273b0fba4f8fb2d751a7f4a
lateralcreativity <lateralcreativity@outlook.com>
2020-05-14 15:59:31 -0500

    All styles seperated into individual components using styled-componenets. Still functions the same as before including the loading... while image is loading

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios'
 import APOD from './components/APOD'
 import Header from './components/Header'
+import Loading from './components/Loading'
 import { BASE_URL, API_KEY } from './constants'
+import styled from 'styled-components';
 import "./App.css";
+
+const StyledApp = styled.div`
+width: 100%;
+min-height: 100vh;
+margin: 0 auto;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+align-items: center;
+`
 
 function App() {
 
@@ -11,19 +23,25 @@ function App() {
 
   useEffect(() => {
     axios.get(`${BASE_URL}?api_key=${API_KEY}`)
-    .then(resolve => {
-      setData(resolve.data)
-    })
-    .catch(error => {
-      debugger
-    })
+      .then(resolve => {
+        setData(resolve.data)
+      })
+      .catch(error => {
+        debugger
+      })
   }, [])
-  if (!data.url) return <h3 className="loading">Loading...</h3>
+
   return (
-    <div className="App">
-      <Header date={data.date}/>
-      <APOD title={data.title} imgUrl={data.url} explanation={data.explanation}/>
-    </div>
+    <StyledApp className="App">
+      {
+        !data.url ? <Loading>Loading...</Loading>
+        :
+        <>
+          <Header date={data.date} />
+          <APOD title={data.title} imgUrl={data.url} explanation={data.explanation} />
+        </>
+      }
+    </StyledApp>
   );
 }
 
--- src/components/APOD.js
+++ src/components/APOD.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledAPOD = styled.div`
+width: 80%;
+margin: 2% 0;
+background-color: #f1f2f6;
+border-radius: 25px;
+display:flex;
+flex-direction: column;
+align-items: center;
+
+img{
+width: 40%;
+border-radius: 15px;
+}
+
+p{
+width: 60%;
+}
+`
 
 export default function APOD(props){
     const { title, imgUrl, explanation} = props;
     return (
-        <div className="apod">
+        <StyledAPOD>
             <h2>{title}</h2>
             <img src={imgUrl} alt=""/>
             <p>{explanation}</p>
-        </div>
+        </StyledAPOD>
     )
 }
 
